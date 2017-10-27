1306340083f59e04cdb62601d748cabdc6dd22f1
StephSoares <smjsoares@hotmail.fr>
2017-10-27 13:48:50 +0000

    My Portfolio V2 (php)

---
+++ script.php
@@ -0,0 +1,7 @@
+<?php
+
+    require_once('./template/head.html');
+    require_once('./presentation.html');
+    require_once('./competences.html');
+    require_once('./portfolio.html');
+    require_once('./template/foot.html');
